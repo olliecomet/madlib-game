cacb4d6e0f2121d876f570d6a5a10b528ed0a5cb
ollie <soulshaymin16@gmail.com>
2019-07-10 21:04:40 -0700

    html, css updates

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const fake = document.getElementById('fake');
+const action = document.getElementById('action');
+const noun = document.getElementById('noun');
+const nameResult = document.getElementById('nameResult');
+const actionResult = document.getElementById('actionResult');
+const nounResult = document.getElementById('nounResult');
+ 
+function input() {
+    nameResult.textContent = fake.value;
+    actionResult.textContent = action.value;
+    nounResult.textContent = noun.value;
+}
+
