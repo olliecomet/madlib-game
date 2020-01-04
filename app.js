a7c9910c55776a7d2560fa00caa1d0f5de03323f
olliecomet <leyechi@gmail.com>
2020-01-03 22:39:53 -0800

    fixed tabbing

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-
 const fake = document.getElementById('fake');
 const action = document.getElementById('action');
 const noun = document.getElementById('noun');
 const nameResult = document.getElementById('nameResult');
 const actionResult = document.getElementById('actionResult');
 const nounResult = document.getElementById('nounResult');
- 
-function input() {
-    nameResult.textContent = fake.value;
-    actionResult.textContent = action.value;
-    nounResult.textContent = noun.value;
-}
 
+function input() {
+  nameResult.textContent = fake.value;
+  actionResult.textContent = action.value;
+  nounResult.textContent = noun.value;
+}
